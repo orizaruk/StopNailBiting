409abf11da0ede66a127ff9255fc113b60d5d9c4
Ori Z <oriz1618@gmail.com>
2025-06-23 18:02:38 +0300

    Implemented the full logic for detecting the biting in a static image.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import mediapipe as mp
-import pprint
+from shapely import Point, Polygon
+from pprint import pp
+
+IMAGE_PATH = "assets/img7.jpg"
 
 hand_model_path = "models/hand_landmarker.task"
 face_model_path = "models/face_landmarker.task"
@@ -18,6 +21,7 @@ FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
 hand_options = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path="models/hand_landmarker.task"),
     running_mode=VisionRunningMode.IMAGE,
+    num_hands=2,
 )
 
 face_options = FaceLandmarkerOptions(
@@ -26,32 +30,68 @@ face_options = FaceLandmarkerOptions(
 )
 
 # Load the image, need to modify for live stream in the future
-mp_image = mp.Image.create_from_file("assets/img2.jpg")
+mp_image = mp.Image.create_from_file(IMAGE_PATH)
 
-with HandLandmarker.create_from_options(hand_options) as hand_landmarker:
-    hand_landmarker_result = hand_landmarker.detect(mp_image)
-    for index, keypoint in enumerate(hand_landmarker_result.hand_landmarks[0]):
-        key_point_coords = (keypoint.x, keypoint.y, keypoint.z)
-        print(f"Hand Landmark {index}: {key_point_coords}")
+# LIST OF THE RELEVANT LANDMARK INDICES
+LIP_INDICES = [
+    308,
+    324,
+    318,
+    402,
+    317,
+    14,
+    87,
+    178,
+    88,
+    95,
+    78,
+    191,
+    80,
+    81,
+    82,
+    13,
+    312,
+    311,
+    310,
+    415,
+    308,
+]
+HAND_INDICES = [4, 3, 8, 7, 12, 11, 16, 15, 20, 19]
 
-with FaceLandmarker.create_from_options(face_options) as face_landmarker:
-    face_landmarker_result = face_landmarker.detect(mp_image)
-    # face_landmarker_result.face_landmarks[0] is the array of normalized_landmarks objects
-    # print result to file
-    result_landmarks_array = face_landmarker_result.face_landmarks[0]
-    lip_landmarks = [
-        0,
-        11,
-        12,
-        13,
-        14,
-        15,
-        16,
-        17,
-    ]  # the landmarks we care about (the ones of the lip in this case)
-    output_filename = "face_landmarker_result_output.txt"
-    output_string = ""
-    for landmark_num in lip_landmarks:
-        landmark_object = result_landmarks_array[landmark_num]
-        landmark_coords = (landmark_object.x, landmark_object.y, landmark_object.z)
-        print(f"Face Landmark {landmark_num}: {landmark_coords}")
+with HandLandmarker.create_from_options(
+    hand_options
+) as hand_landmarker, FaceLandmarker.create_from_options(
+    face_options
+) as face_landmarker:
+    # Run the inference process for the hand landmark model
+    hand_result = hand_landmarker.detect(mp_image)
+
+    # Only if we detected hands, let us continue with the face inference (for efficiency, if no hands, no need.)
+    if hand_result.hand_landmarks:
+        # Run the Face Inference
+        face_result = face_landmarker.detect(mp_image)
+        # Continue the checks only if a face has been detected
+        if face_result.face_landmarks:
+            # Extract only the relevant lip landmarks from the face scan, and store them in an array in the format of [(x1,y1), (x2,y2),...]
+            face_landmarks_array = face_result.face_landmarks[0]
+            lip_points_coords_list = []
+            for index in LIP_INDICES:
+                landmark_info = face_landmarks_array[index]
+                lip_points_coords_list.append((landmark_info.x, landmark_info.y))
+
+            # Create the Polygon object from the sequence of points
+            precise_polygon = Polygon(lip_points_coords_list)
+            # Create a buffered polygon to smooth it out and allow increasing the size of it for fine-tuning
+            SENSITIVITY = 0.01
+            buffered_polygon = precise_polygon.buffer(SENSITIVITY)
+
+            # Now we need to check for each of the relevant hand landmarks if it's inside the polygon, for all the hands
+            for hand in hand_result.hand_landmarks:
+                # For each relevant landmark, let us check if it's within the polygon
+                for hand_landmark_index in HAND_INDICES:
+                    landmark_info = hand[hand_landmark_index]
+                    # Create a Point object using the (x,y) coords
+                    point = Point(landmark_info.x, landmark_info.y)
+                    # CHECK IF THE POINT IS WITHIN THE POLYGON, IF IT IS - NAIL BITING!
+                    if buffered_polygon.contains(point):
+                        print("BITING DETECTED!")
